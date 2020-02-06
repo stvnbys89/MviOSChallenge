541c8baae0e2117e52a5fa0a99ba70baf42e81e3
Steven <stvnbys89@gmail.com>
2020-02-06 11:55:15 +0800

    - added constant file to set entry and expiry lifetime for cache

---
+++ MindvalleyiOSChallenge/Utils/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  MindvalleyiOSChallenge
+//
+//  Created by Steven on 06/02/2020.
+//  Copyright © 2020 StvnBys. All rights reserved.
+//
+
+import UIKit
+
+public class Constant{
+    //lifetime by hour as default
+    public static let cacheLifetime = 1.0
+    //maximumEntryCount 50 as default
+    public static let cacheEntry = 50
+
+}
